73265f6042fe316d20246395912f0ad18be5e74e
ChristianThuranira <christianthuranira@gmail.com>
2025-02-01 14:29:38 +0300

    app

--- server/app.py
+++ server/app.py
@@ -5,15 +5,20 @@ from flask_marshmallow import Marshmallow
 from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
+
+# ? Correct Database Path
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fitness.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = 'your_secret_key'  # Change this for security
 
+# ? Initialize Extensions
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 jwt = JWTManager(app)
-CORS(app)  # Enable CORS for Postman and frontend requests
+CORS(app)  # Allow CORS for Postman & Frontend
 
+# ? Import Models & Routes After Initialization
+from models import *
 from routes import *
 
 if __name__ == '__main__':
