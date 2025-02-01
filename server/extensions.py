a9656b3e05aec36b4019206f936d168d3f93fe93
ChristianThuranira <christianthuranira@gmail.com>
2025-02-01 14:48:21 +0300

    new extensions

--- server/extensions.py
+++ server/extensions.py
@@ -3,7 +3,7 @@ from flask_marshmallow import Marshmallow
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 
-# Initialize the extensions
+# Initialize extensions
 db = SQLAlchemy()
 ma = Marshmallow()
 jwt = JWTManager()
