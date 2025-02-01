2ec0deed397605b67ba9d307f40968e9f1fa4652
ChristianThuranira <christianthuranira@gmail.com>
2025-02-01 13:44:51 +0300

    app

--- server/app.py
+++ server/app.py
@@ -1,23 +1,20 @@
-#!/usr/bin/env python3
+from flask import Flask
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from flask_jwt_extended import JWTManager
 
-# Standard library imports
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fitness.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'your_secret_key'  # Change this for security
 
-# Remote library imports
-from flask import request
-from flask_restful import Resource
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+jwt = JWTManager(app)
+CORS(app)  # Enable CORS for Postman and frontend requests
 
-# Local imports
-from config import app, db, api
-# Add your model imports
-
-
-# Views go here!
-
-@app.route('/')
-def index():
-    return '<h1>Project Server</h1>'
-
+from routes import *
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
-
+    app.run(debug=True, port=5555)
---
+++ server/migrations/versions/ec1b005e29b4_case_study.py
@@ -0,0 +1,24 @@
+"""Case study
+
+Revision ID: ec1b005e29b4
+Revises: 
+Create Date: 2025-02-01 13:18:50.268051
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ec1b005e29b4'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
