d0d1bda61dd21f2ca56ba5445cf798f31308d94d
ChristianThuranira <christianthuranira@gmail.com>
2025-02-02 12:36:40 +0300

    dashboard update

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,13 +1,15 @@
-import React, { useContext } from "react";
-import { AuthContext } from "../contexts/AuthContext";
-import { useHistory } from "react-router-dom";
+// /src/components/Dashboard.js
+
+import React from 'react';
+import { useAuth } from '../contexts/AuthContext';
+import { useHistory } from 'react-router-dom';
 
 const Dashboard = () => {
-  const { user, logout } = useContext(AuthContext);
+  const { user, logout } = useAuth();
   const history = useHistory();
 
   if (!user) {
-    history.push("/login");
+    history.push('/'); // Redirect to login if user is not logged in
     return null;
   }
 
