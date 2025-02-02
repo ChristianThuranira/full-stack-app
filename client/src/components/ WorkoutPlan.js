902bcdd95e5ba2a8875d5bbdee5fbe8be4dcba2c
ChristianThuranira <christianthuranira@gmail.com>
2025-02-02 12:38:01 +0300

    workout update

---
+++ client/src/components/ WorkoutPlan.js
@@ -0,0 +1,15 @@
+// /src/components/WorkoutPlan.js
+
+import React from 'react';
+
+const WorkoutPlan = () => {
+  return (
+    <div>
+      <h2>Workout Plan</h2>
+      {/* Display workout plans */}
+      <p>Workout plan details go here.</p>
+    </div>
+  );
+};
+
+export default WorkoutPlan;
